46f8657fe4aca436a13cd4fc873676cc02bcbac8
Tanja Sennhauser <tanja.sennhauser@nobel-web-engineering.ch>
2017-05-08 18:48:36 +0200

    added first files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>myNotes</title>
+		<meta name="description" content="myNotes App" />
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" media="all">
+		<!--[if IE]>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<![endif]-->
+	</head>
+	<body>
+		<main>
+      <h1>Welcome to myNotes</h1>
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
