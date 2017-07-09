22c5ac03c38112e4f01b3b623c24d241ce51716e
Tanja Sennhauser <tanja.sennhauser@nobel-web-engineering.ch>
2017-07-09 23:21:29 +0200

    completed edit function

--- public/js/controllers/createController.js
+++ public/js/controllers/createController.js
@@ -2,34 +2,36 @@
 
 (function ($) {
     const rest = window.services.restClient;
+    let id = 0;
 
     if (window.location.hash) {
-
-        let id = 0;
-        const note = rest.getNoteById(window.location.hash.substr(1)).done(function (note) {
+        id = window.location.hash.substr(1);
+        const note = rest.getNoteById(id).done(function (note) {
             $("#title").val(note.title);
             $("#description").val(note.description);
             $("#dueDate").val(note.dueDate);
             $("#importance").val(note.importance);
         });
-
-        //Edit note item
-        $(document).on("click", ".btn_edit", function () {
-            editNote();
-        });
-
-        //Save note
-        $("#saveNote").click(function() {
-            if (id === 0) {
-                rest.addNote(note).then(() =>{
-                    window.location.replace("index.html");
-                });
-            } else {
-                rest.editNote(note);
-            }
-
-        });
     }
+
+    //Edit note item
+    $(document).on("click", ".btn_edit", function () {
+            editNote();
+    });
+
+    //Save note
+    $("#saveNote").click(function() {
+        if (id === 0) {
+            console.log('save');
+            rest.addNote($("#title").val(), $("#description").val(), $("#importance").val(), $("#dueDate").val()).then(() =>{
+                window.location.replace("index.html");
+            })
+        } else {
+            console.log('edit');
+            rest.editNote(id, $("#title").val(), $("#description").val(), $("#importance").val(), $("#dueDate").val());
+                window.location.replace("index.html");
+        }
+    });
 
     //Styleswitcher
     function getDefaultStyle() {
